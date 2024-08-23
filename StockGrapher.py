31086d81b606297ef6b7a652223dcd52d97a4903
MaxBoving <146762130+MaxBoving@users.noreply.github.com>
2024-08-22 21:38:25 -0700

    changes to UX implemented in stylesheet and statsimages, minor bug patches(data checks & exception handling) in StockGrapher

--- StockGrapher.py
+++ StockGrapher.py
@@ -13,16 +13,79 @@ from matplotlib.figure import Figure
 import io
 from flask import Response
 
+import yfinance as yf
+import pandas as pd
+
 def get_stats(ticker):
     Ticker = yf.Ticker(ticker)
-    Ticker.recommendations_summary
-    Ticker.recommendations
-    Ticker.recommendations_summary
-    df = pd.DataFrame(Ticker.upgrades_downgrades).head(10)
+    df = pd.DataFrame(Ticker.upgrades_downgrades).head(5)
+    df2 = pd.DataFrame(Ticker.recommendations).head(5)    
+    df = df.drop(columns=["FromGrade"])
     
-    #df = df.drop(columns=
-    result = df.to_html()
-    return result
+    def convert_period(period_str):
+        num_months = int(period_str.replace('m', ''))
+        
+        abs_months = abs(num_months)
+        
+        if num_months == 0:
+            return 'current month'
+        elif num_months == -1:
+            return '1 month ago'
+        else:
+            return f'{abs_months} months ago'
+
+    if 'period' in df2.columns:
+        df2['period'] = df2['period'].apply(convert_period)   
+    
+    def highlight_action(val):
+        color = {
+            'main': 'lightblue',
+            'up': 'lightgreen',
+            'reit': 'lightyellow',
+            '': 'white'  
+        }.get(val, 'white')
+        return f'background-color: {color}; color: black;'
+    
+    def highlight_grade(val):
+        color = {
+            'Outperform': 'darkgreen',
+            'Buy': 'lightgreen',
+            'hold': 'gray',
+            'sell': 'lightred',
+            'Underperform': 'green',
+            'Underweight': 'red',
+            'Overweight': 'darkred',
+        }.get(val, 'white')
+        return f'background-color: {color}; color: black;'
+    
+    def apply_gradient_styles(df):
+        numeric_columns = ['strongBuy', 'buy', 'hold', 'sell', 'strongSell']
+        styled_df2 = df.style.background_gradient(subset=numeric_columns, cmap='BuGn')
+        return styled_df2.to_html()
+
+    def apply_styles(df):
+        if 'Action' in df.columns:
+            styled_df = df.style.applymap(highlight_action, subset=['Action'])
+        if 'ToGrade' in df.columns:    
+            styled_df = styled_df.applymap(highlight_grade, subset=['ToGrade'])
+            
+        return styled_df.to_html()
+    
+    result = apply_styles(df)
+    result2 = apply_gradient_styles(df2)
+    
+    list_of_tables = [result, result2]
+    
+    
+    return list_of_tables
+
+
+def remove_newlines(l_of_t):
+    for html in l_of_t:
+        for item in html:
+            item.strip('\n')
+    return l_of_t
+
 
 
 def IsValid(ticks):
@@ -77,7 +140,7 @@ def CreateVis(stockdict):
         ax.set_xlim(start_date - pd.Timedelta(days=1) / 4, end_date + pd.Timedelta(days=1) / 4)
         ax.set_ylim(min_close * 0.9, max_close * 1.1)
         ax.set_xlabel('Dates')
-        ax.set_ylabel('Close Price')
+        ax.set_ylabel('Close Price($)')
         plt.title(ticker)
 def save_plot_to_memory(stockdict):
         buffer = io.BytesIO()  # Create a buffer to hold the PNG image
--- server.py
+++ server.py
@@ -20,15 +20,18 @@ def get_Stock():
     tickers = request.args.get('ticker')
     per = request.args.get('periods')
     tickers, period  = StockGrapher.GetInput(tickers, per)
+    result = []
     for t in tickers:
-        stats = StockGrapher.get_stats(t)
-        result = []
-        result.append(stats)
-    
-    #ticker = request.form['ticker']
-    #period = request.form['period']
-    # Redirect to the /stockimages route with the selected ticker and period
-    return render_template('statsimages.html', ticker=tickers ,period=period, result=result)
+        try:
+            stats = StockGrapher.get_stats(t)
+            result.append(stats)
+        except Exception as e:
+            print(f"Error retrieving stats for {t}: {e}")
+            result.append("no data available")
+    for r in result:
+        clean_html = StockGrapher.remove_newlines(r)
+    # Redirect to the /stockimages route with the selected ticker and period and stats
+    return render_template('statsimages.html', ticker=tickers ,period=period, result=clean_html)
 
     
 
@@ -36,17 +39,16 @@ def get_Stock():
 def get_image():
     tickers = request.args.get('ticker')
     per = request.args.get('period')
+    
     tickers, period  = StockGrapher.GetInput(tickers, per)
     data = StockGrapher.FetchData(tickers, period)
     clean = StockGrapher.CleanData(data)
     plot_buffer = StockGrapher.save_plot_to_memory(clean)
     plt.close()
     response = make_response(plot_buffer.getvalue())
-
     # Set the appropriate content type and headers
     response.headers['Content-Type'] = 'image/png'
     response.headers['Content-Disposition'] = 'inline; filename=plot.png'
-    
     return response
 
     
